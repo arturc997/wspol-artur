2eaab37e5d9a3f360f9daec6a6a1d631f12e53c7
arturc997 <arturc997@interia.pl>
2017-10-19 14:27:01 +0200

    implementacja pierszego programu w języku c

---
+++ pierwszy_program/program.c
@@ -0,0 +1,21 @@
+  GNU nano 2.2.6                                        Plik: pierwszy_program                                                                             Zmodyfikowany 
+
+#include <studio.h> /*bibloteka standardowa input-output */
+
+/* y = f(x) */
+/*     f(0 => 0 */
+
+int main() /*funkcja głowna programu */
+
+/* int -> typ zwracanej wartości 9int - wartosc całkowita) */ 
+
+{	
+	puts("pierwszy program"")  /* puts -  funkcja z bibloteki standardowej*/
+				   /* puts -> punt string */
+				   /* wyswietlajaca ciag znakow */
+				   /* automatycznie przechodzi do nowej lini */
+
+	putchar('g'):
+        putchar('\n'): /*znak konca lini*/
+        putchar('\t'): /*tabulator*/
+        putchar('\r'):
